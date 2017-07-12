121ef372e8c1a4a0932866a137dc2797d2f29937
Lionel London <lionel.london@ligo.org>
2017-07-12 15:46:51 +0100

    adding setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Import useful things
+from distutils.core import setup
+from setuptools import find_packages
+
+#
+setup(name='kerr',
+      version='1.0',
+      description='Python Utilities for BH QNMs',
+      author='Lionel London',
+      author_email='lionel.london@ligo.org',
+      packages=find_packages(),
+      include_package_data=True,
+      package_dir={'kerr': 'kerr'},
+      url='https://github.com/llondon6/kerr_public/',
+      download_url='https://github.com/llondon6/kerr_public/archive/master.zip',
+      install_requires=['h5py','numpy','scipy','matplotlib'],
+     )
